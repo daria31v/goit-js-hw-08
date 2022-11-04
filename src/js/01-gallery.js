a6870b03b6bf6d0baa658a7747bfd231c70a6ada
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2022-11-04 11:50:42 +0100

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// 
 import { galleryItems } from './gallery-items';
+const containerGallery = document.querySelector('.gallery');
+
+const creatListMarkup = galleryItems.map(({
+  preview,
+  original,
+  description
+}) => `
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      alt=${description}
+    />
+  </a>`).join('');
+
+containerGallery.insertAdjacentHTML('beforeend', creatListMarkup);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
 // Change code below this line
 
 console.log(galleryItems);
