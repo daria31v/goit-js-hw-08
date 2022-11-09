ac711bb465840af93dcb813e196d12816488af80
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2022-11-09 23:57:31 +0100

    Update 03-feedback.js
    виправлення

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,116 +1,49 @@
 import throttle from "lodash.throttle";
 
-
-const STORAGE_KEY = 'feedback-form-state';
 const feedbackForm = document.querySelector('.feedback-form');
+const KEY = 'feedback-form-state';
+const formData = {};
 
-let formData = {};
-let email = document.querySelector('input');
-let message = document.querySelector('textarea');
+let emailEl = document.querySelector('input');
+let messageEl = document.querySelector('textarea');
 
 
-// dataFromLocalStorage - проверка при загрузке страницы
 dataFromLocalStorage();
 
 feedbackForm.addEventListener('submit', onFormSubmit);
+feedbackForm.addEventListener('input', throttle(onInputUser, 500));
 
-feedbackForm.addEventListener('input', throttle(onMessageInput, 500));
+// пеервірка LS при перезавантаженні сторінки
+function dataFromLocalStorage() {
+    if (localStorage.getItem(KEY)){
+    const savedData = JSON.parse(localStorage.getItem(KEY) || '');
+          
+    feedbackForm.elements.email.value = savedData.email;
+    feedbackForm.elements.message.value = savedData.message;
+} 
+}  
 
+// відправув даних у LS
+function onInputUser() {
+    formData.email = feedbackForm.elements.email.value;
+    formData.message = feedbackForm.elements.message.value;
+    localStorage.setItem(KEY, JSON.stringify(formData));
+}
+
+// зчитування даних з LS
 function onFormSubmit(evt) {
     evt.preventDefault();
-    console.log("send form");
-
+    if (feedbackForm.elements.email.value && feedbackForm.elements.message.value) {
+    dataSend({
+        email: emailEl.value, message: messageEl.value
+    });
     evt.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
- 
-}
-
-function onMessageInput(evt) {
-    
-    formData[evt.target.name] = evt.target.value;
-    console.log(formData[evt.target.name]);
-
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-    
-}
-
-function dataFromLocalStorage() {
-    
-    const saveData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-       
-    if (saveData) {
-        email.value = saveData.email;
-        console.log (email.value);
-
-        message.value = saveData.message;
-        console.log( message.value);
-        }
-}
+    localStorage.removeItem(KEY);
+    }
+} 
+function dataSend({ email, message } = {}) {
+    return console.log("send form", { email, message })
+}    
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// НЕ УДАЛЯТЬ   НЕ УДАЛЯТЬ   НЕ УДАЛЯТЬ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-// feedbackForm.addEventListener('submit', onFormSubmit);
-// message.addEventListener('input', throttle(onMessageInput, 500));
-
-// feedbackForm.addEventListener('input', e => {
-   
-//     formData[e.target.name] = e.target.value;
-//     // console.log('Data',formData);
-    
-//     const formDataSend = JSON.stringify(formData);
-//     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));   
-//     console.log('forma', formDataSend)
-
-//     const saveData = localStorage.getItem(STORAGE_KEY);
-//     console.log('saveData', saveData);
-
-//     const parseData = JSON.parse(saveData);
-//     console.log(parseData);
-
-// });
-
-// function onFormSubmit(evt) {
-//     evt.preventDefault();
-//     // console.log("send form");
-//     evt.currentTarget.reset();
-//     localStorage.removeItem(STORAGE_KEY);
-
-// }
-
-// function onMessageInput(evt) {
-//     const message = evt.target.value;
-//     localStorage.setItem(STORAGE_KEY, message);
-    
-
-
-//     // console.log(message)
-     
-// }
-
-// function messageFromLocolStorage() {
-//     const savedMessage = localStorage.getItem(STORAGE_KEY);
-//     if (savedMessage) {
-//         // console.log(savedMessage);
-//         message.value = savedMessage;
-//     }
-    
-// }
